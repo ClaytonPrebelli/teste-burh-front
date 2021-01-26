0b8816dcb69b13eadfeafb8db0a011eb987b5191
Clayton Prebelli <clayton.pires@aluno.unip.br>
2021-01-25 22:57:46 -0300

    criado header e iniciado footer

--- biblioteca-app/src/app/app.module.ts
+++ biblioteca-app/src/app/app.module.ts
@@ -1,20 +1,27 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component, NgModuleDecorator } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './views/templates/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon';
+import { FooterComponent } from './views/templates/footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NgModule
+    FooterComponent,
+
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
