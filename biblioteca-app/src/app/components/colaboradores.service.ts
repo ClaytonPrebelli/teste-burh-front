c681ebad8d699e213a77dbb4096a04c781ee3f6a
Clayton Prebelli <clayton.pires@aluno.unip.br>
2021-01-31 13:51:59 -0300

    corrigido rota pelo enviroment

--- biblioteca-app/src/app/components/colaboradores.service.ts
+++ biblioteca-app/src/app/components/colaboradores.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment';
 import { ActivatedRoute } from '@angular/router';
 import { Colaborador } from './colaboradores.model';
 import { Injectable } from '@angular/core';
@@ -7,12 +8,12 @@ import { HttpClient } from '@angular/common/http';
 import { catchError, map } from 'rxjs/operators';
 import { Route } from '@angular/compiler/src/core';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class ColaboradoresService {
-  baseUrl = "https://crudcrud.com/api/152a12e9128f4f18bb0477c8850a358b/Colaborador"
-  constructor(private snackBar: MatSnackBar, private http: HttpClient, private route: ActivatedRoute) { }
+    constructor(private snackBar: MatSnackBar, private http: HttpClient, private route: ActivatedRoute) { }
 
   showMessage(msg: string, isError: boolean = false): void {
     this.snackBar.open(msg, 'X', {
@@ -22,22 +23,22 @@ export class ColaboradoresService {
     })
   }
   create(colaborador: Colaborador): Observable<Colaborador> {
-    return this.http.post<Colaborador>(this.baseUrl, colaborador).pipe(map((obj) => obj), catchError(e => this.errorHandler(e)))
+    return this.http.post<Colaborador>(environment.apiUlr, colaborador).pipe(map((obj) => obj), catchError(e => this.errorHandler(e)))
   }
   read(): Observable<Colaborador[]> {
-    return this.http.get<Colaborador[]>(this.baseUrl)
+    return this.http.get<Colaborador[]>(environment.apiUlr)
   }
   readById(_id: string | null): Observable<Colaborador> {
-    const url = `${this.baseUrl}/${_id}`
+    const url = `${environment.apiUlr}/${_id}`
     return this.http.get<Colaborador>(url).pipe(map((obj) => obj), catchError(e => this.errorHandler(e)))
   }
   update(colaborador: Colaborador): Observable<Colaborador> {
-    const url = `${this.baseUrl}/${colaborador._id}`
+    const url = `${environment.apiUlr}/${colaborador._id}`
     console.log(url)   
     return this.http.put<Colaborador>(url, colaborador).pipe(map((obj) => obj), catchError(e => this.errorHandler(e)))
   }
   delete(_id:number): Observable<Colaborador>{
-    const url = `${this.baseUrl}/${_id}`
+    const url = `${environment.apiUlr}/${_id}`
     return this.http.delete<Colaborador>(url).pipe(map((obj) => obj), catchError(e => this.errorHandler(e)))
   }
 
--- biblioteca-app/src/environments/environment.ts
+++ biblioteca-app/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUlr: "https://crudcrud.com/api/152a12e9128f4f18bb0477c8850a358b/Colaborador"
 };
 
 /*
